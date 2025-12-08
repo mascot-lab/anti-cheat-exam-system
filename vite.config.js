c98d79ace90ff30b12526e8ca587b15bd7e64525
秀鲤鱼 <69128446+cyprinus-carpio@users.noreply.github.com>
2025-12-08 23:30:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 60902,
-    }
+  base: '/anti-cheat-exam-system/',
+  plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  build: {
+    outDir: 'build', 
+    target: 'es2020',
+  }
 })
