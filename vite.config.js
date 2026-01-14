9675fad43cb9faba4d68675d231ea12c3076a076
秀鲤鱼 <69128446+cyprinus-carpio@users.noreply.github.com>
2026-01-14 22:54:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/anti-cheat-exam-system/',
+  base: './',
   plugins: [react()],
   define: {
     global: 'globalThis',
@@ -13,5 +13,6 @@ export default defineConfig({
     target: 'es2020',
   }
 })
+
 
 
